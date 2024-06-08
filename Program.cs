aa4d2a4393324db713c0d652f315577727ebda15
KalandarovST <kalandarovtillo78@gmail.com>
2024-06-08 18:46:44 +0300

    Hisob

--- Program.cs
+++ Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+int radius ;
+decimal pi = 3.14m;
+Console.Write("Qiymat kiriting:");
+string inputSon = Console.ReadLine();
+radius = Convert.ToInt32(inputSon);
+decimal hisob = pi*radius*radius;
+Console.WriteLine(hisob);
